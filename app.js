d51a57d19b2d849eb70a4050bdfa859566dcb876
Tim Park <tim@arcticempire.ca>
2016-07-07 00:33:35 -0400

    Change wget calls to request calls, and scrape VIEWSTATE from pages with cheerio.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 var fs = require('fs');
 var async = require('async');
-var sf = require('jsforce');
+var request = require('request');
+var cheerio = require('cheerio');
 var Converter = require('csvtojson').Converter;
 var converter = new Converter({});
-var child_process = require('child_process');
+var sf = require('jsforce');
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport(process.env.SMTPACCT);
 
@@ -19,78 +20,102 @@ var added = { Account: 0, Contact: 0, Opportunity: 0 };
 var yesterday = (function(d){d.setDate(d.getDate()-1); return d})(new Date);
 var date = ("0" + yesterday.getDate()).slice(-2) + '/' + ("0" + (yesterday.getMonth()+1)).slice(-2) + '/' + yesterday.getFullYear(); // 25/06/2016
 //date = "25/06/2016";
-// Hackathon WARNING: Trusting the env vars to not be malicious
-var wget1 = "wget --keep-session-cookies --save-cookies cookies.txt --post-data '__VIEWSTATE=" + process.env.CH_VIEWSTATE1 + "&ctl00$bodyContentContainer$SignInControl$EmailAddress=" + process.env.CH_USERNAME + "&ctl00$bodyContentContainer$SignInControl$Password=" + process.env.CH_PASSWORD + "&ctl00$bodyContentContainer$SignInControl$btnSignIn=Sign In' " + process.env.CH_URL + "/en/SignIn.aspx -O /dev/null";
-var wget2 = "wget --keep-session-cookies --load-cookies cookies.txt --post-data '__VIEWSTATE=" + process.env.CH_VIEWSTATE2 + "&ctl00$bodyContentContainer$txtFromDate=" + date + "&ctl00$bodyContentContainer$txtToDate=" + date + "&ctl00$bodyContentContainer$btnDownloadData.x=20&ctl00$bodyContentContainer$btnDownloadData.y=13' " + process.env.CH_URL + "/en/Admin/MCDonations_DataDownload.aspx -O CharityDataDownload.csv";
 
-child_process.exec(wget1, function (err, stdout, stderr) {
-  if (err) { return output(err); }
-  child_process.exec(wget2, function (err, stdout, stderr) {
+var reqOptions = [
+  { method:"GET",  url:process.env.CH_URL + "/en/SignIn.aspx" },
+  { method:"POST", url:process.env.CH_URL + "/en/SignIn.aspx", jar:true, followAllRedirects:true, form:
+    { 'ctl00$bodyContentContainer$SignInControl$EmailAddress':process.env.CH_USERNAME,
+      'ctl00$bodyContentContainer$SignInControl$Password':process.env.CH_PASSWORD,
+      'ctl00$bodyContentContainer$SignInControl$btnSignIn':'Sign In' } },
+  { method:"GET",  url:process.env.CH_URL + "/en/Admin/MCDonations_DataDownload.aspx", jar:true },
+  { method:"POST", url:process.env.CH_URL + "/en/Admin/MCDonations_DataDownload.aspx", jar:true, form:
+    { 'ctl00$bodyContentContainer$txtFromDate':date,
+      'ctl00$bodyContentContainer$txtToDate':date,
+      'ctl00$bodyContentContainer$btnDownloadData.x':20,
+      'ctl00$bodyContentContainer$btnDownloadData.y':13 } }
+];
+
+var data = "";
+var viewstate = "";
+var q = async.queue(function (options, callback) {
+  if (options.form !== undefined) { options.form['__VIEWSTATE'] = viewstate; }
+  request(options, function (err, response, html) {
+    if (err || response.statusCode !== 200) { return output(err); }
+    var $ = cheerio.load(html);
+    if (typeof $('input#__VIEWSTATE')[0] === 'object') { viewstate = $('input#__VIEWSTATE')[0].attribs.value; }
+    data = html;
+    callback();
+  });
+});
+
+q.drain = function() { processData(data); }
+
+for (var i in reqOptions) { q.push(reqOptions[i]); }
+
+function processData(data) {
+  converter.fromString(data, function(err, results) {
     if (err) { return output(err); }
-    converter.fromFile("./CharityDataDownload.csv", function(err, results) {
+    conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD + process.env.SF_ACCESS_TOKEN, function(err, user) {
       if (err) { return output(err); }
-      conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD + process.env.SF_ACCESS_TOKEN, function(err, user) {
-        if (err) { return output(err); }
-        var asyncTasks = [];
-        results.forEach(function(result) {
-          if (result['DONOR COMPANY NAME'] === undefined) { return; }
-          var account = { Name:'General' };
-          if ((result['DONOR COMPANY NAME'] != '') && (result['DONOR COMPANY NAME'] != 'ANON')) {
-            account.Name = result['DONOR COMPANY NAME'];
-          }
+      var asyncTasks = [];
+      results.forEach(function(result) {
+        if (result['DONOR COMPANY NAME'] === undefined) { return; }
+        var account = { Name:'General' };
+        if ((result['DONOR COMPANY NAME'] != '') && (result['DONOR COMPANY NAME'] != 'ANON')) {
+          account.Name = result['DONOR COMPANY NAME'];
+        }
 
-          var contact = {};
-          contact.FirstName = capitalizeFirstLetter(result['DONOR FIRST NAME']);
-          if (contact.FirstName === '') { contact.FirstName = 'ANON'; }
-          contact.LastName = capitalizeFirstLetter(result['DONOR LAST NAME']);
-          if (contact.LastName === '') { contact.LastName = 'ANON'; }
-          contact.Email = result['DONOR EMAIL ADDRESS'];
-          if (contact.Email === '') { contact.Email = 'ANON'; }
-          if (contact.Email.indexOf("@") === -1) { contact.Email += '@ANON.COM'; }
-          contact.Email = contact.Email.toLowerCase();
-          //contact.Account = ""; // Lookup(Account)
-          contact.MailingStreet = result['DONOR ADDRESS 1'] + '\n' + result['DONOR ADDRESS 2'];
-          contact.MailingCity = result['DONOR CITY'];
-          contact.MailingState = result['DONOR PROVINCE/STATE'];
-          contact.MailingPostalCode = result['DONOR POSTAL/ZIP CODE'];
-          contact.MailingCountry = result['DONOR COUNTRY'];
+        var contact = {};
+        contact.FirstName = capitalizeFirstLetter(result['DONOR FIRST NAME']);
+        if (contact.FirstName === '') { contact.FirstName = 'ANON'; }
+        contact.LastName = capitalizeFirstLetter(result['DONOR LAST NAME']);
+        if (contact.LastName === '') { contact.LastName = 'ANON'; }
+        contact.Email = result['DONOR EMAIL ADDRESS'];
+        if (contact.Email === '') { contact.Email = 'ANON'; }
+        if (contact.Email.indexOf("@") === -1) { contact.Email += '@ANON.COM'; }
+        contact.Email = contact.Email.toLowerCase();
+        //contact.Account = ""; // Lookup(Account)
+        contact.MailingStreet = result['DONOR ADDRESS 1'] + '\n' + result['DONOR ADDRESS 2'];
+        contact.MailingCity = result['DONOR CITY'];
+        contact.MailingState = result['DONOR PROVINCE/STATE'];
+        contact.MailingPostalCode = result['DONOR POSTAL/ZIP CODE'];
+        contact.MailingCountry = result['DONOR COUNTRY'];
 
-          var opportunity = {};
-          opportunity.Name = result['TRANSACTION NUMBER'];
-          //opportunity.Donor_Name__c = ""; // Lookup(Contact)
-          opportunity.StageName = 'Closed Won';
-          opportunity.canh__Payment_Method__c = result['PAYMENT METHOD'];
-          opportunity.Amount = result['AMOUNT'];
-          opportunity.CloseDate = result['DONATION DATE'];
-          opportunity.Receive_Date__c = result['DONATION DATE'];
-          opportunity.canh__Fee__c = result['FEE'];
-          opportunity.canh__In_Honour__c = result['IN HONOUR OF'];
-          opportunity.canh__In_Memory__c = result['IN MEMORY OF'];
-          opportunity.canh__Honouree__c = result['HONOUREE'];
-          opportunity.Description = result['MESSAGE TO CHARITY'];
-          opportunity.canh__Donation_Source__c = result['DONATION SOURCE'];
+        var opportunity = {};
+        opportunity.Name = result['TRANSACTION NUMBER'];
+        //opportunity.Donor_Name__c = ""; // Lookup(Contact)
+        opportunity.StageName = 'Closed Won';
+        opportunity.canh__Payment_Method__c = result['PAYMENT METHOD'];
+        opportunity.Amount = result['AMOUNT'];
+        opportunity.CloseDate = result['DONATION DATE'];
+        opportunity.Receive_Date__c = result['DONATION DATE'];
+        opportunity.canh__Fee__c = result['FEE'];
+        opportunity.canh__In_Honour__c = result['IN HONOUR OF'];
+        opportunity.canh__In_Memory__c = result['IN MEMORY OF'];
+        opportunity.canh__Honouree__c = result['HONOUREE'];
+        opportunity.Description = result['MESSAGE TO CHARITY'];
+        opportunity.canh__Donation_Source__c = result['DONATION SOURCE'];
 
-          if (debug > 1) {
-            console.log("-----------------------");
-            console.log(account);
-            console.log(contact);
-            console.log(opportunity);
-          }
+        if (debug > 1) {
+          console.log("-----------------------");
+          console.log(account);
+          console.log(contact);
+          console.log(opportunity);
+        }
 
-          asyncTasks.push(function(callback) { createData("Account", "Name", account, callback, contact, "AccountId"); });
-          asyncTasks.push(function(callback) { createData("Contact", "Email", contact, callback, opportunity, "Donor_Name__c"); });
-          asyncTasks.push(function(callback) { createData("Opportunity", "Name", opportunity, callback, null, null); });
-        });
-        async.series(asyncTasks, function(){
-          var text = "New Accounts:      " + added.Account + "\n" +
-                     "New Contacts:      " + added.Contact + "\n" +
-                     "New Opportunities: " + added.Opportunity;
-          output(text);
-        });
+        asyncTasks.push(function(callback) { createData("Account", "Name", account, callback, contact, "AccountId"); });
+        asyncTasks.push(function(callback) { createData("Contact", "Email", contact, callback, opportunity, "Donor_Name__c"); });
+        asyncTasks.push(function(callback) { createData("Opportunity", "Name", opportunity, callback, null, null); });
+      });
+      async.series(asyncTasks, function(){
+        var text = "New Accounts:      " + added.Account + "\n" +
+                   "New Contacts:      " + added.Contact + "\n" +
+                   "New Opportunities: " + added.Opportunity;
+        output(text);
       });
     });
   });
-});
+}
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
